6be5bd6991c25024ed475a2c29535795b63c1105
Marwan Al-Obaidi <marwan1alobaidi@live.fi>
2023-04-27 16:55:26 +0300

    Basic functionality added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const SYMBOLS = ["cherry", "lemon", "orange", "plum", "bell", "bar", "seven"];
+
+function SlotMachine() {
+  const [reels, setReels] = useState([0, 0, 0]);
+
+  function spin() {
+    const newReels = [
+      Math.floor(Math.random() * SYMBOLS.length),
+      Math.floor(Math.random() * SYMBOLS.length),
+      Math.floor(Math.random() * SYMBOLS.length),
+    ];
+    setReels(newReels);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="SlotMachine">
+      <div className="Reel">{SYMBOLS[reels[0]]}</div>
+      <div className="Reel">{SYMBOLS[reels[1]]}</div>
+      <div className="Reel">{SYMBOLS[reels[2]]}</div>
+      <button onClick={spin}>Spin</button>
     </div>
   );
 }
 
-export default App;
+export default SlotMachine;
